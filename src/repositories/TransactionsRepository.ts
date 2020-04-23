9486af0c2724421cb7cff1b74c414bfe1c7c6ec0
Abilio Soares Coelho <abiliocoelho@gmail.com>
2020-04-23 18:55:03 -0300

    :sparkles challenge finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -18,26 +18,29 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): { transactions: Transaction[]; balance: Balance } {
-    const balance = this.getBalance();
-    return { transactions: this.transactions, balance };
+  public all(): Transaction[] {
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    const inc = this.transactions.reduce((total, elemento): number => {
-      if (elemento.type === 'income') {
-        return (total += elemento.value);
-      }
-      return total;
-    }, 0);
-
-    const out = this.transactions.reduce((total, elemento): number => {
-      if (elemento.type === 'outcome') {
-        return (total += elemento.value);
-      }
-      return total;
-    }, 0);
-    return { income: inc, outcome: out, total: inc - out };
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += transaction.value;
+        }
+        if (transaction.type === 'outcome') {
+          accumulator.outcome += transaction.value;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
   public create({ title, type, value }: CreateTrasactionDTO): Transaction {
